6b399b9e73dd5ee57560aca32ed4753072870e6b
Unknown <elenalunagarcia@gmail.com>
2018-11-12 17:06:34 +0100

    Generate detections script

--- generate_detections.m
+++ generate_detections.m
@@ -4,6 +4,7 @@ clc; clear; close all force;
 %% PATH and PARAMETERS
 
 baseFolder = pwd;
+detections_path = [baseFolder '/Results/Detections/'];
 datasets_path = [baseFolder '/Datasets/'];
 datasets_names_raw = dir(datasets_path);
 datasets_names = {};
@@ -20,10 +21,10 @@ for i=1:length(datasets_names_raw)
 end
 
 
-
 %% LOAD DATA
 %addpath(genpath('utils'));
 gt_data= {};
+det_data= {};
 for i = 1:numel(datasets_names) %i = dataset
     
     sequences_names_raw=dir([datasets_path datasets_names{i}]);
@@ -40,12 +41,44 @@ for i = 1:numel(datasets_names) %i = dataset
     end
     
     for j = 1:numel(sequences_names) %j = sequence
-        gt_data{i} = dlmread(fullfile(datasets_path, datasets_names{i}, '/gt/gt.txt'));
-        frames = unique(data(:, 1));
-        n_frames = length(frames);
-        IDs = unique(data(:,2));
-        n_people = length(IDs);
+        gt_data{i,j} = dlmread(fullfile(datasets_path, datasets_names{i}, sequences_names{j},'/gt/gt.txt'));
+    end
+    
+end
+
+%% GENERATE DETECTION FROM GT
+
+% ------- GT format -----------------------------------
+%   1    2   3  4  5  6    7      8          9
+% frame  id  x  y  w  h  active  type  visibility_ratio
+
+% ------- Dets format ----------------------------
+%   1    2   3  4  5  6  7   8          9
+% frame  -1  x  y  w  h  1  type  visibility_ratio
+
+det_data = {};
+option = 'gt';
+
+for i = 1:numel(datasets_names) %i = dataset
+   
+    for j = 1:numel(sequences_names) %j = sequence
+        index_not_ignored = (gt_data{i,j}(:,7) == 1);
+        det_data{i,j} =  gt_data{i,j}(index_not_ignored,:); %supress non-active gt bboxes
+        det_data{i,j}(:,2) = -1; % suppres id -> -1
+        det_data{i,j} = sortrows(det_data{i,j}); % sort by first column (frame)
+        
+%         img_path = [datasets_path datasets_names{i} '/' sequences_names{j} '/img1/000001.jpg'];
+%         img= imread(img_path);
+%         img2 = insertShape(img, 'Rectangle', det_data{i,j}(1:20,3:6));
+%         imshow(img2);
+%         
+        detections_file = [detections_path datasets_names{i} '/' option '/' sequences_names{j}  '.txt'];
+        dlmwrite(detections_file,  det_data{i,j});
+
+    
     end
     
     
 end
+
+
