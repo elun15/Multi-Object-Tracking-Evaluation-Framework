7461fd7f3ccf2aa62e4d659e7a4854b11165c176
Unknown <elenalunagarcia@gmail.com>
2019-02-13 10:05:13 +0100

    Display functions

--- Display/m1_versus_m2.m
+++ Display/m1_versus_m2.m
@@ -1,6 +1,6 @@
 clc; clear all; close all;
 
-% For a specific tracker and sequence, plot MOTA measure with each detections
+% For a specific tracker and dataet, plot 2 measures with each detections
 
 % {'IFD1','IDP','IDR','Recall','Precision','False Alarm Rate','GT Tracks','Mostly Tracked',
 %     'Partially Tracked','Mostly Lost','False Positives','False Negatives','ID Switches',
@@ -25,16 +25,20 @@ clc; clear all; close all;
 
 
 addpath(genpath('./External'));
+addpath(genpath('./Mat'));
+addpath(genpath('./Utils/display'));
 
 load('./Mat/sequences.mat'); % Sequences info
-load('Mat/results_tracking.mat'); % Struct with the computed results
+load('./Mat/results_eval_tracking.mat'); % Struct with the computed results
 
-list_trackers = {'MATLAB','SORT'};
+list_trackers = {'MATLAB','SORT','GOG'};
+% list_trackers = {'GOG'};
+
 dataset = ['Visdrone2018_train']; % MOT16_train  MOT16_train Visdrone2018_train
 %sequence = ['MOT16-02'];
 % index =strcmp({sequences.(dataset).name},sequence); %index of sequence in the dataset
 
-list_metrics = [4 5];%[4 15 2 ;% Numbers of metrics to plot
+list_metrics = [ 15 16];%[4 15 2 ;% Numbers of metrics to plot
 list_detections = fieldnames(results_tracking);
 
 position_subplot=1;
@@ -52,49 +56,56 @@ for t = 1:numel(list_trackers)
             list_sequences = {results_tracking.(list_detections{d}).(list_trackers{t}).(dataset).name};
             
             for s = 1: numel(list_sequences) % concatenate data from all sequences in the dataset
-                
-                disp(['Tracker ' list_trackers{t}  ' detection ' list_detections{d} ' dataset ' dataset ' sequence ' ...
-                    results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).name ...
-                    ' metric ' results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_allClass.names{list_metrics(m)} ]);
-                
-                
-                if isempty(metrics_allSequences)
+                if s<11
+                    disp(['Tracker ' list_trackers{t}  ' detection ' list_detections{d} ' dataset ' dataset ' sequence ' ...
+                        results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).name ...
+                        ' metric ' results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_allClass.names{list_metrics(m)} ]);
                     
-                    metrics_allSequences= struct(results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_perClass);
-                else
-                    metrics_allSequences(end+1) = results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_perClass;
-                 end
-                %metsBenchmak = evaluateBenchmark(allMets, world)
-                
+                    if isempty(metrics_allSequences)
+                        
+                        metrics_allSequences= struct(results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_perClass);
+                    else
+                        number_newElements = size(results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_perClass,2);
+                        metrics_allSequences(end+1:end+number_newElements) = results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(s).metrics_perClass;
+                    end
+                end
             end
             
-            
             metrics_dataset = evaluateBenchmark(metrics_allSequences, 0);
-%             path_results = results_tracking.(list_detections{d}).(list_trackers{t}).(dataset)(index).path;
-%             load(fullfile(path_results, 'allClassMets.mat'));
-%             
-             data(m,d) = metrics_dataset.m(list_metrics(1));
+            
+            data(m,d) = metrics_dataset.m(list_metrics(m));
             % [mota_d1 mota__d2,
             % motp_d1 motp_d2]
             
         end
         
-        
     end
-    
     
     subplot(numel(list_trackers),1,position_subplot);  % Comment for separately plotting
     
+    colors = distinguishable_colors(size(data,2));
     for i = 1:size(data,2)
-        plot(data(1,i),data(2,i),'x','MarkerSize',10,'LineWidth',3);
+        plot(data(1,i),data(2,i),'x','MarkerSize',18,'LineWidth',4,'Color',colors(i,:));
         hold on;
     end
-    
+   
     title([list_trackers{t} ' tracker evaluation in ' dataset ' dataset ' ],'Interpreter', 'none','FontSize', 16);
-    xlabel(metrics_dataset.names(list_metrics(1)),'FontSize', 16);
-    ylabel(metrics_dataset.names(list_metrics(2)),'FontSize', 16);
+    xlabel(metrics_dataset.names(list_metrics(1)),'FontSize', 20);
+    ylabel(metrics_dataset.names(list_metrics(2)),'FontSize', 20);
+    
+    if (list_metrics == [15 16]) % MOTA MOTP
+        axis([-0 100 -0 100])
+       
+        
+    elseif (list_metrics == [4 5]) % precision recall
+        axis([0 100 0 100])
+        
+    elseif (list_metrics == [2 3]) % IDP IDF
+        axis([0 100 0 100])
+    end
+    
     leg = legend(list_detections);
-    set(leg, 'Interpreter', 'none','FontSize', 10);
+    set(leg, 'Interpreter', 'none','FontSize', 16);
     
     position_subplot = position_subplot+1;
 end
